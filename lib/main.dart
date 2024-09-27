f2d6b9808d7d7f30afef6a5c43e206b8283aedab
carldlanor <carl.fornal27@gmail.com>
2024-09-27 11:54:26 -0500

    initiate sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
+  // await dotenv.load(fileName: ".env");
   WidgetsFlutterBinding.ensureInitialized();
   if (kIsWeb) {
     await Firebase.initializeApp(
@@ -28,27 +31,70 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   @override
+  // void initState() {
+  //   print(dotenv.env['APIKEY']);
+  //   super.initState();
+  // }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: const Text('Home Screen'),
+        title: const Text('Hanapbuhay'),
         centerTitle: true,
         backgroundColor: Colors.grey[400],
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {},
-          child: const Icon(
-            Icons.abc,
-            color: Colors.white,
-          ),
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Colors.blue,
-          ),
-        ),
-      ),
-      backgroundColor: Colors.grey[800],
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8.0, 0.0, 8.0, 8.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15.0,
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+            ),
+            const SizedBox(
+              height: 15.0,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                'Log in',
+                style: TextStyle(fontSize: 15.0, color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                'Sign up',
+                style: TextStyle(fontSize: 15.0, color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+            ),
+          ]),
+      backgroundColor: Colors.grey[200],
     ));
   }
 }
